72a670c4617768e351c895cfaeb3ef1b30430119
danielbernalb <danielbernalb@gmail.com>
2023-07-01 22:15:18 -0500

    Cambios2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <Arduino.h>
 
-//#include "C:\esp\ESP8266_RTOS_SDK\components\esp8266\include\esp_wifi.h"
-//#include "C:\esp\ESP8266_RTOS_SDK\components\wpa_supplicant\include\esp_supplicant\esp_wpa2.h"
-//#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\FreeRTOS.h"
 
-//#include "C:\esp32\esp-idf\components\esp_wifi\include"
+// INCLUDE ESP32
 
 #include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi.h"
 #include "C:\esp32\esp-idf\components\wpa_supplicant\esp_supplicant\include\esp_wpa2.h"
@@ -15,36 +12,46 @@
 #include "C:\esp32\esp-idf\components\freertos\FreeRTOS-Kernel\include\freertos\task.h"
 //#include "freertos/event_groups.h"
 #include "C:\esp32\esp-idf\components\freertos\FreeRTOS-Kernel\include\freertos\event_groups.h"
-//#include "esp_wifi.h"
-//#include "esp_wpa2.h"
+
 //#include "esp_event.h"
-
-//#include "C:\esp32\esp-idf\components\esp_netif\include\esp_netif.h"
-
 #include "C:\esp32\esp-idf\components\esp_event\include\esp_event.h"
+//#include "esp_netif.h"
+#include "C:\esp32\esp-idf\components\esp_netif\include\esp_netif.h"
+
 //#include "esp_log.h"
 #include "C:\esp32\esp-idf\components\log\include\esp_log.h"
 //#include "esp_system.h"
 #include "C:\esp32\esp-idf\components\esp_system\include\esp_system.h"
 //#include "nvs_flash.h"
 #include "C:\esp32\esp-idf\components\nvs_flash\include\nvs_flash.h"
-//#include "esp_netif.h"
 
 //#include "esp_err.h"
 #include "C:\esp32\esp-idf\components\esp_common\include\esp_err.h"
 //#include "esp_wifi_types.h"
 #include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi_types.h"
 //#include "esp_event.h"
-//#include "C:\esp32\esp-idf\components\esp_event\include\esp_event.h
+#include "C:\esp32\esp-idf\components\esp_event\include\esp_event.h"
 //#include "esp_private/esp_wifi_private.h"
 #include "C:\esp32\esp-idf\components\esp_wifi\include\esp_private\esp_wifi_private.h"
 //#include "esp_wifi_default.h"
 #include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi_default.h"
 
+#include <WiFi.h>
+#include "WiFiGeneric.h"
+
+//#include "C:\esp32\pruebas\WPA2enterprise\sdkconfig"
+
+//#include "C:\esp32\esp-idf\components\esp_hw_support\include\esp_private\adc_share_hw_ctrl.h" //NO FUNCIONO
+//#include "C:\esp32\esp-idf\components\esp_wifi\src\wifi_init.c" // NO FUNCIONO
+
 ////////////////////////////////
 
-//#include "C:\esp32\esp-idf\components\esp_hw_support\include\esp_private\adc_share_hw_ctrl.h"
-//#include "C:\esp32\esp-idf\components\esp_wifi\src\wifi_init.c"
+//INCLUDE ESP8266
+
+//#include "C:\esp\ESP8266_RTOS_SDK\components\esp8266\include\esp_wifi.h"
+//#include "C:\esp\ESP8266_RTOS_SDK\components\wpa_supplicant\include\esp_supplicant\esp_wpa2.h"
+//#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\FreeRTOS.h"
+
 
 ///////////////////////////////
 
@@ -58,6 +65,53 @@
 #define EXAMPLE_EAP_USERNAME "prueba1"
 #define EXAMPLE_EAP_PASSWORD "daniel2022"
 
+#define WIFI_INIT_CONF() { \
+    .osi_funcs = &g_wifi_osi_funcs, \
+    .wpa_crypto_funcs = g_wifi_default_wpa_crypto_funcs, \
+    .static_rx_buf_num = CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM,\
+    .dynamic_rx_buf_num = CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM,\
+    .tx_buf_type = CONFIG_ESP32_WIFI_TX_BUFFER_TYPE,\
+    .static_tx_buf_num = WIFI_STATIC_TX_BUFFER_NUM,\
+    .dynamic_tx_buf_num = WIFI_DYNAMIC_TX_BUFFER_NUM,\
+    .cache_tx_buf_num = WIFI_CACHE_TX_BUFFER_NUM,\
+    .csi_enable = WIFI_CSI_ENABLED,\
+    .ampdu_rx_enable = WIFI_AMPDU_RX_ENABLED,\
+    .ampdu_tx_enable = WIFI_AMPDU_TX_ENABLED,\
+    .amsdu_tx_enable = WIFI_AMSDU_TX_ENABLED,\
+    .nvs_enable = WIFI_NVS_ENABLED,\
+    .nano_enable = WIFI_NANO_FORMAT_ENABLED,\
+    .rx_ba_win = WIFI_DEFAULT_RX_BA_WIN,\
+    .wifi_task_core_id = WIFI_TASK_CORE_ID,\
+    .beacon_max_len = WIFI_SOFTAP_BEACON_MAX_LEN, \
+    .mgmt_sbuf_num = WIFI_MGMT_SBUF_NUM, \
+    .feature_caps = g_wifi_feature_caps, \
+    .sta_disconnected_pm = WIFI_STA_DISCONNECTED_PM_ENABLED,  \
+    .espnow_max_encrypt_num = CONFIG_ESP_WIFI_ESPNOW_MAX_ENCRYPT_NUM, \
+    .magic = WIFI_INIT_CONFIG_MAGIC\
+}
+
+/*
+#define WIFI_INIT_IDFexample() { \
+    .event_handler = &esp_event_send, \
+    .osi_funcs = NULL, \
+    .qos_enable = WIFI_QOS_ENABLED,\
+    .ampdu_rx_enable = WIFI_AMPDU_RX_ENABLED,\
+    .rx_ba_win = WIFI_AMPDU_RX_BA_WIN,\
+    .rx_ampdu_buf_num = WIFI_AMPDU_RX_AMPDU_BUF_NUM,\
+    .rx_ampdu_buf_len = WIFI_AMPDU_RX_AMPDU_BUF_LEN,\
+    .rx_max_single_pkt_len = WIFI_RX_MAX_SINGLE_PKT_LEN,\
+    .rx_buf_len = WIFI_HW_RX_BUFFER_LEN,\
+    .amsdu_rx_enable = WIFI_AMSDU_RX_ENABLED,\
+    .rx_buf_num = CONFIG_ESP8266_WIFI_RX_BUFFER_NUM,\
+    .rx_pkt_num = CONFIG_ESP8266_WIFI_RX_PKT_NUM,\
+    .left_continuous_rx_buf_num = CONFIG_ESP8266_WIFI_LEFT_CONTINUOUS_RX_BUFFER_NUM,\
+    .tx_buf_num = CONFIG_ESP8266_WIFI_TX_PKT_NUM,\
+    .nvs_enable = WIFI_NVS_ENABLED,\
+    .nano_enable = 0,\
+    .wpa3_sae_enable = WIFI_WPA3_ENABLED, \
+    .magic = WIFI_INIT_CONFIG_MAGIC\
+};
+*/
 
 //* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t wifi_event_group;
@@ -88,6 +142,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
   }
 }
 
+//static void initialise_wifi(void)
 void initialise_wifi()
 {
   ESP_ERROR_CHECK(esp_netif_init());
@@ -96,7 +151,7 @@ void initialise_wifi()
   sta_netif = esp_netif_create_default_wifi_sta();
   assert(sta_netif);
   Serial.println("Test 6");  
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  wifi_init_config_t cfg = WIFI_INIT_CONF();
   Serial.println("Test 7");
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
   Serial.println("Test 8");
