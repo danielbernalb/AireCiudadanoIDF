338b0b67cd36fbce5a6604475b78038289a79fb8
danielbernalb <danielbernalb@gmail.com>
2023-07-04 17:40:55 -0500

    Test3

--- src/main.cpp
+++ src/main.cpp
@@ -1,203 +1,135 @@
 #include <Arduino.h>
 
+#include <string.h>
+#include <stdlib.h>
 
-// INCLUDE ESP32
+// #include "freertos/FreeRTOS.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\FreeRTOS.h"
+// #include "freertos/task.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\task.h"
 
-#include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi.h"
-#include "C:\esp32\esp-idf\components\wpa_supplicant\esp_supplicant\include\esp_wpa2.h"
+// #include "freertos/event_groups.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\event_groups.h"
+// #include "esp_wifi.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\esp8266\include\esp_wifi.h"
+// #include "esp_wpa2.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\wpa_supplicant\include\esp_supplicant\esp_wpa2.h"
+// #include "esp_event.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\esp_event\include\esp_event.h"
+// #include "esp_log.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\log\include\esp_log.h"
+// #include "esp_system.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\esp8266\include\esp_system.h"
+// #include "nvs_flash.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\nvs_flash\include\nvs_flash.h"
+// #include "tcpip_adapter.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\tcpip_adapter\include\tcpip_adapter.h"
+// #include "esp_event_loop.h"
+#include "C:\esp\ESP8266_RTOS_SDK\components\esp_event\include\esp_event_loop.h"
 
-//#include "freertos/FreeRTOS.h"
-#include "C:\esp32\esp-idf\components\freertos\FreeRTOS-Kernel\include\freertos\FreeRTOS.h"
-//#include "freertos/task.h"
-#include "C:\esp32\esp-idf\components\freertos\FreeRTOS-Kernel\include\freertos\task.h"
-//#include "freertos/event_groups.h"
-#include "C:\esp32\esp-idf\components\freertos\FreeRTOS-Kernel\include\freertos\event_groups.h"
+/* The examples use simple WiFi configuration that you can set via
+   'make menuconfig'.
 
-//#include "esp_event.h"
-#include "C:\esp32\esp-idf\components\esp_event\include\esp_event.h"
-//#include "esp_netif.h"
-#include "C:\esp32\esp-idf\components\esp_netif\include\esp_netif.h"
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 
-//#include "esp_log.h"
-#include "C:\esp32\esp-idf\components\log\include\esp_log.h"
-//#include "esp_system.h"
-#include "C:\esp32\esp-idf\components\esp_system\include\esp_system.h"
-//#include "nvs_flash.h"
-#include "C:\esp32\esp-idf\components\nvs_flash\include\nvs_flash.h"
+   You can choose EAP method via 'make menuconfig' according to the
+   configuration of AP.
+*/
 
-//#include "esp_err.h"
-#include "C:\esp32\esp-idf\components\esp_common\include\esp_err.h"
-//#include "esp_wifi_types.h"
-#include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi_types.h"
-//#include "esp_event.h"
-#include "C:\esp32\esp-idf\components\esp_event\include\esp_event.h"
-//#include "esp_private/esp_wifi_private.h"
-#include "C:\esp32\esp-idf\components\esp_wifi\include\esp_private\esp_wifi_private.h"
-//#include "esp_wifi_default.h"
-#include "C:\esp32\esp-idf\components\esp_wifi\include\esp_wifi_default.h"
-
-#include <WiFi.h>
-#include "WiFiGeneric.h"
-
-//#include "C:\esp32\pruebas\WPA2enterprise\sdkconfig"
-
-//#include "C:\esp32\esp-idf\components\esp_hw_support\include\esp_private\adc_share_hw_ctrl.h" //NO FUNCIONO
-//#include "C:\esp32\esp-idf\components\esp_wifi\src\wifi_init.c" // NO FUNCIONO
-
-////////////////////////////////
-
-//INCLUDE ESP8266
-
-//#include "C:\esp\ESP8266_RTOS_SDK\components\esp8266\include\esp_wifi.h"
-//#include "C:\esp\ESP8266_RTOS_SDK\components\wpa_supplicant\include\esp_supplicant\esp_wpa2.h"
-//#include "C:\esp\ESP8266_RTOS_SDK\components\freertos\include\freertos\FreeRTOS.h"
-
-
-///////////////////////////////
-
-//#define EXAMPLE_WIFI_SSID "SED CORPORATIVO"
-//#define EXAMPLE_EAP_ID "wiedctabora"
-//#define EXAMPLE_EAP_USERNAME "wiedctabora"
-//#define EXAMPLE_EAP_PASSWORD "OZ4\"Xbk0"
+// #define EXAMPLE_WIFI_SSID "SED CORPORATIVO"
+// #define EXAMPLE_EAP_ID "wiedctabora"
+// #define EXAMPLE_EAP_USERNAME "wiedctabora"
+// #define EXAMPLE_EAP_PASSWORD "OZ4\"Xbk0"
 
 #define EXAMPLE_WIFI_SSID "TPwpa2"
 #define EXAMPLE_EAP_ID "prueba1"
 #define EXAMPLE_EAP_USERNAME "prueba1"
 #define EXAMPLE_EAP_PASSWORD "daniel2022"
 
-#define WIFI_INIT_CONF() { \
-    .osi_funcs = &g_wifi_osi_funcs, \
-    .wpa_crypto_funcs = g_wifi_default_wpa_crypto_funcs, \
-    .static_rx_buf_num = CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM,\
-    .dynamic_rx_buf_num = CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM,\
-    .tx_buf_type = CONFIG_ESP32_WIFI_TX_BUFFER_TYPE,\
-    .static_tx_buf_num = WIFI_STATIC_TX_BUFFER_NUM,\
-    .dynamic_tx_buf_num = WIFI_DYNAMIC_TX_BUFFER_NUM,\
-    .cache_tx_buf_num = WIFI_CACHE_TX_BUFFER_NUM,\
-    .csi_enable = WIFI_CSI_ENABLED,\
-    .ampdu_rx_enable = WIFI_AMPDU_RX_ENABLED,\
-    .ampdu_tx_enable = WIFI_AMPDU_TX_ENABLED,\
-    .amsdu_tx_enable = WIFI_AMSDU_TX_ENABLED,\
-    .nvs_enable = WIFI_NVS_ENABLED,\
-    .nano_enable = WIFI_NANO_FORMAT_ENABLED,\
-    .rx_ba_win = WIFI_DEFAULT_RX_BA_WIN,\
-    .wifi_task_core_id = WIFI_TASK_CORE_ID,\
-    .beacon_max_len = WIFI_SOFTAP_BEACON_MAX_LEN, \
-    .mgmt_sbuf_num = WIFI_MGMT_SBUF_NUM, \
-    .feature_caps = g_wifi_feature_caps, \
-    .sta_disconnected_pm = WIFI_STA_DISCONNECTED_PM_ENABLED,  \
-    .espnow_max_encrypt_num = CONFIG_ESP_WIFI_ESPNOW_MAX_ENCRYPT_NUM, \
-    .magic = WIFI_INIT_CONFIG_MAGIC\
-}
-
-/*
-#define WIFI_INIT_IDFexample() { \
-    .event_handler = &esp_event_send, \
-    .osi_funcs = NULL, \
-    .qos_enable = WIFI_QOS_ENABLED,\
-    .ampdu_rx_enable = WIFI_AMPDU_RX_ENABLED,\
-    .rx_ba_win = WIFI_AMPDU_RX_BA_WIN,\
-    .rx_ampdu_buf_num = WIFI_AMPDU_RX_AMPDU_BUF_NUM,\
-    .rx_ampdu_buf_len = WIFI_AMPDU_RX_AMPDU_BUF_LEN,\
-    .rx_max_single_pkt_len = WIFI_RX_MAX_SINGLE_PKT_LEN,\
-    .rx_buf_len = WIFI_HW_RX_BUFFER_LEN,\
-    .amsdu_rx_enable = WIFI_AMSDU_RX_ENABLED,\
-    .rx_buf_num = CONFIG_ESP8266_WIFI_RX_BUFFER_NUM,\
-    .rx_pkt_num = CONFIG_ESP8266_WIFI_RX_PKT_NUM,\
-    .left_continuous_rx_buf_num = CONFIG_ESP8266_WIFI_LEFT_CONTINUOUS_RX_BUFFER_NUM,\
-    .tx_buf_num = CONFIG_ESP8266_WIFI_TX_PKT_NUM,\
-    .nvs_enable = WIFI_NVS_ENABLED,\
-    .nano_enable = 0,\
-    .wpa3_sae_enable = WIFI_WPA3_ENABLED, \
-    .magic = WIFI_INIT_CONFIG_MAGIC\
-};
-*/
-
-//* FreeRTOS event group to signal when we are connected & ready to make a request */
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t wifi_event_group;
-
-/* esp netif object representing the WIFI station */
-static esp_netif_t *sta_netif = NULL;
 
 /* The event group allows multiple bits for each event,
    but we only care about one event - are we connected
    to the AP with an IP? */
-const int CONNECTED_BIT = BIT0;
+#define WIFI_CONNECTED_BIT BIT0
 
 static const char *TAG = "example";
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data)
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
 {
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+  {
     esp_wifi_connect();
     ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
-  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    esp_wifi_connect();
-    xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+  }
+  else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+  {
+    system_event_sta_disconnected_t *event = (system_event_sta_disconnected_t *)event_data;
+
     ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+  }
+  else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+  {
+    xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
     ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
   }
 }
 
-//static void initialise_wifi(void)
+// static void initialise_wifi(void)
 void initialise_wifi()
 {
-  ESP_ERROR_CHECK(esp_netif_init());
+  tcpip_adapter_init();
   wifi_event_group = xEventGroupCreate();
   ESP_ERROR_CHECK(esp_event_loop_create_default());
-  sta_netif = esp_netif_create_default_wifi_sta();
-  assert(sta_netif);
-  Serial.println("Test 6");  
-  wifi_init_config_t cfg = WIFI_INIT_CONF();
-  Serial.println("Test 7");
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  Serial.println("Test 8");
   ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-  Serial.println("Test 9");
   ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-  Serial.println("Test 10");
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-  Serial.println("Test 11");
-  
-  wifi_config_t conf;
-  
+  //  wifi_config_t wifi_config = {
+  //      .sta = {
+  //          .ssid = EXAMPLE_WIFI_SSID,
+  //      },
+  //  };
   ESP_LOGI(TAG, "SSID: %s", EXAMPLE_WIFI_SSID);
   ESP_LOGI(TAG, "Identity: %s", EXAMPLE_EAP_ID);
   ESP_LOGI(TAG, "Password: %s", EXAMPLE_EAP_PASSWORD);
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &conf));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
   esp_wifi_sta_wpa2_ent_clear_identity();
   esp_wifi_sta_wpa2_ent_clear_username();
   esp_wifi_sta_wpa2_ent_clear_password();
   esp_wifi_sta_wpa2_ent_clear_new_password();
   esp_wifi_sta_wpa2_ent_clear_ca_cert();
   esp_wifi_sta_wpa2_ent_clear_cert_key();
-  //    esp_wifi_sta_wpa2_ent_set_disable_time_check(false);
-  Serial.println("Test 12");
+  esp_wifi_sta_wpa2_ent_set_disable_time_check(false);
 
-  ESP_ERROR_CHECK(esp_wifi_sta_wpa2_ent_set_identity((uint8_t*)EXAMPLE_EAP_ID, strlen(EXAMPLE_EAP_ID)));
-  ESP_ERROR_CHECK(esp_wifi_sta_wpa2_ent_set_username((uint8_t*)EXAMPLE_EAP_USERNAME, strlen(EXAMPLE_EAP_USERNAME)));
-  ESP_ERROR_CHECK(esp_wifi_sta_wpa2_ent_set_password((uint8_t*)EXAMPLE_EAP_PASSWORD, strlen(EXAMPLE_EAP_PASSWORD)));
+  ESP_ERROR_CHECK(esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EXAMPLE_EAP_ID, strlen(EXAMPLE_EAP_ID)));
   ESP_ERROR_CHECK(esp_wifi_sta_wpa2_ent_enable());
   ESP_ERROR_CHECK(esp_wifi_start());
-  Serial.println("Wifi start");
-  Serial.println("wifi_init_sta finished.");
-
+  // Serial.println("Wifi start");
+  ESP_LOGI(TAG, "Wifi start");
+  ESP_LOGI(TAG, "wifi_init_sta finished.");
 }
 
 static void wpa2_enterprise_example_task(void *pvParameters)
 {
-  esp_netif_ip_info_t ip;
-  memset(&ip, 0, sizeof(esp_netif_ip_info_t));
+  tcpip_adapter_ip_info_t ip;
+  memset(&ip, 0, sizeof(tcpip_adapter_ip_info_t));
   vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-  while (1) {
+  vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+  while (1)
+  {
     vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-    if (esp_netif_get_ip_info(sta_netif, &ip) == 0) {
+    if (esp_netif_get_ip_info(sta_netif, &ip) == 0)
+    {
       ESP_LOGI(TAG, "~~~~~~~~~~~");
       ESP_LOGI(TAG, "IP:" IPSTR, IP2STR(&ip.ip));
       ESP_LOGI(TAG, "MASK:" IPSTR, IP2STR(&ip.netmask));
@@ -225,7 +157,7 @@ void setup()
 
   initialise_wifi();
 }
-//void app_main()
+// void app_main()
 void loop()
 {
   xTaskCreate(&wpa2_enterprise_example_task, "wpa2_enterprise_example_task", 4096, NULL, 5, NULL);
